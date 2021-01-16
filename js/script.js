b740486a609359b06369378f4cd3206788536541
watarmeli <watar@ukr.net>
2021-01-16 21:48:06 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function filterArray(numbers, value) {
+  // Пиши код ниже этой строки
+	const newArray = [];
+  for (const number of numbers) {
+  	if (number > value) {
+      newArray.push(number);
+    }
+  }
+
+
+	return newArray;
+  // Пиши код выше этой строки
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
