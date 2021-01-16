0e07509138896092e04dd09f282a3927504e543f
watarmeli <watar@ukr.net>
2021-01-16 19:22:18 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function getExtremeElements(array) {
+  // Пиши код ниже этой строки
+  return [array[0], array[array.length - 1]];
+
+
+
+  // Пиши код выше этой строки
+}
+
+console.log(getExtremeElements([1, 2, 3, 4, 5]));
+console.log(getExtremeElements(['Земля', 'Марс', 'Венера']));
+console.log(getExtremeElements(['яблоко', 'персик', 'груша', 'банан']));
