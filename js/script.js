317f4ca7ef8ef793def19f578c3e003658d0563b
watarmeli <watar@ukr.net>
2021-01-16 21:06:05 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function findLongestWord(string) {
+  // Пиши код ниже этой строки
+  const toArr = string.split(' ');
+
+  let longestWord = toArr[0];
+
+  for (const word of toArr) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  
+  return longestWord;
+
+  // Пиши код выше этой строки
+}
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+console.log(findLongestWord('Google do a roll'));
+console.log(findLongestWord('May the force be with you'));
