6086476935a9bdf021f18101fb1dfd0b7d71d3e7
watarmeli <watar@ukr.net>
2021-01-16 19:02:08 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Стартовый код
+const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+// Пиши код ниже этой строки
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[fruits.length - 1];
+
+console.log(firstElement);
+console.log(secondElement);
+console.log(lastElement);
