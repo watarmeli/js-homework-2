81cc7d0e2cf678a240afd9422e07f54f0eebf152
watarmeli <watar@ukr.net>
2021-01-18 02:31:19 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function calculateTotalPrice(order) {
+    let total = 0;
+    // Пиши код ниже этой строки
+    for (const orderItem of order) {
+        total += orderItem;
+    }
+  
+    // Пиши код выше этой строки
+    return total;
+  }
+  
+  console.log(calculateTotalPrice([12, 85, 37, 4]));
+  console.log(calculateTotalPrice([164, 48, 291]));
+  console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+  console.log(calculateTotalPrice([]));
